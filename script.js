e9ad278ed8c9d9ed0e8e54ebd90357e79ae22baf
Alicia <dfmalicia@gmail.com>
2023-11-29 19:20:47 +0100

    result fixed

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById("clicker").addEventListener("click", function() {updateSeconds(); counter()});
 
-var count = 0;
 var stopCounter = false;
+var count = 0;
+var initialSeconds;
 function updateSeconds() {
     let time = document.getElementById('time');
     let seconds = parseInt(time.innerHTML, 10);
+    initialSeconds = seconds;
     let intervalId = setInterval(function () {
         seconds--;
-
         if (seconds >= 0) {
             time.innerHTML = seconds;
         } else if(stopCounter === false){
@@ -23,6 +24,8 @@ function counter() {
 }
 
 function displayResult() {
+    let time = document.getElementById('time');
+    let seconds = parseInt(time.innerHTML, 10);
     let result = document.getElementById("res");
     result.innerHTML = count/10;
 }
